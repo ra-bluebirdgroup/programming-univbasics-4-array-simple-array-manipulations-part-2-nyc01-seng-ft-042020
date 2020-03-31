22721b7510af53db7eb7fb52a437ecbdbc292b1e
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-31 22:20:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,16 +3,21 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-  array.insert(element)  
+  array.insert(element)
 end
 
-def using_uniq
+def using_uniq(array)
+  array.uniq
+
 end
 
-def using_flatten
+def using_flatten(array)
+  array.flatten
+
 end
 
-def using_delete
+def using_delete(array, value)
+  array.delete(value)
 end
 
 def using_delete_at
